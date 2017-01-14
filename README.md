a1b3fd8cb61643d8bbd069f8c7ed5738aec347ad
Tran Hoang Hiep <oono@Trans-MacBook-Pro.local>
2017-01-15 05:16:50 +0700

    First Commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Orionsoft GraphQL Server Boilerplate
+
+Installation
+
+```sh
+$ git clone https://github.com/orionsoft/server-boilerplate [name of your app]
+$ cd [name of your app]
+$ rm -rf .git
+$ yarn
+$ meteor npm install --save bcrypt
+$ meteor
+```
---
+++ server/imports/apollo/index.js
@@ -0,0 +1,22 @@
+import {createApolloServer} from 'meteor/apollo'
+import {makeExecutableSchema} from 'graphql-tools'
+import {loadSchema, getSchema} from 'graphql-loader'
+import {initAccounts} from 'meteor/nicolaslopezj:apollo-accounts'
+import cors from 'cors'
+import typeDefs from './schema'
+import resolvers from './resolvers'
+
+// Load all accounts related resolvers and type definitions into graphql-loader
+initAccounts({})
+
+// Load all your resolvers and type definitions into graphql-loader
+loadSchema({typeDefs, resolvers})
+
+// Gets all the resolvers and type definitions loaded in graphql-loader
+const schema = makeExecutableSchema(getSchema())
+
+createApolloServer({schema}, {
+  configServer (graphQLServer) {
+    graphQLServer.use(cors())
+  }
+})
